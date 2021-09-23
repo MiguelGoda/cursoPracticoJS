6dac8cbf8ce13d93b8422ead276bdd62bd8599fc
MiguelGoda <miguel_g14@hotmail.com>
2021-09-23 18:48:00 -0400

    primeros paso

---
+++ calculo.js
@@ -0,0 +1,31 @@
+console.group("cuadrados")
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden " + ladoCuadrado +"cm");
+
+const perimetroCuadrado = ladoCuadrado * 4;
+console.log("El perimetro del cuadrado es " + perimetroCuadrado +"cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("El area del cuadrado es " + areaCuadrado +"cm^2");
+
+console.groupEnd()
+
+console.group("Triangulos")
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+const alturaTriangulo = 5.5;
+
+console.log("Los lados del triangulo miden "
++ ladoTriangulo1
++ "cm, " + ladoTriangulo2
++ "cm, " + baseTriangulo
++ "cm");
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo
+console.log("El area del triangulo es " + perimetroTriangulo + "cm");
+
+const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+console.log("El area del triangulo es " + areaTriangulo + "cm^2");
+console.groupEnd();
