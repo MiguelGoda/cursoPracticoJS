400d208a95b130559c9626f52dd33b10f94d6994
MiguelGoda <miguel_g14@hotmail.com>
2021-09-28 16:39:47 -0400

    agregando el switch del descuento

--- descuentos.js
+++ descuentos.js
@@ -11,14 +11,41 @@ function CalcularPrecioConDescuento(precio, descuento) {
   return precioConDescuento;
 }
 
+const coupons = [
+  "JuanDC_es_Batman",
+  "pero_no_le_digas_a_nadie",
+  "es_un_secreto"
+];
+
 function onClickButtonPriceDiscount() {
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
 
 
-  const inputDiscount = document.getElementById("InputDiscount")
-  const discountValue = inputDiscount.value;
+  const inputCoupon = document.getElementById("InputCoupon")
+  const couponValue = inputCoupon.value;
 
-  const precioConDescuento = CalcularPrecioConDescuento(priceValue, discountValue);
+  let descuento;
+
+  switch(couponValue) {
+    case coupons[0]: //"JuanDC_es_Batman"
+      descuento = 15;
+    break;
+    case coupons[1]: //"pero_no_le_digas_a_nadie"
+      descuento = 30;
+    break;
+    case coupons[2]: //"es_un_secreto"
+      descuento = 25;
+    break;
+  }
+
+  const precioConDescuento = CalcularPrecioConDescuento(priceValue, descuento);
+
+  const resultP = document.getElementById("ResultP");
+
+  resultP.innerText = "El resultado es: $" + precioConDescuento
 
 }
+
+
+
