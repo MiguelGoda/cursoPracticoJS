137bf3c4b6c2d33834e6d8fb76d05e8a542d6b88
MiguelGoda <miguel_g14@hotmail.com>
2021-10-04 20:39:47 -0400

    media ponderacion

---
+++ ponderacion.js
@@ -0,0 +1,39 @@
+const materias = [
+    {
+        nombreMateria: "Programacion",
+        nota: 6,
+        credito: 1
+    },
+    {
+        nombreMateria: "Calculo",
+        nota: 7,
+        credito: 1,
+    },
+    {
+        nombreMateria: "POO",
+        nota: 5,
+        credito: 1,
+    },
+    {
+        nombreMateria: "Estadisticas",
+        nota: 7,
+        credito: 1,
+    },
+    {
+        nombreMateria: "BD",
+        nota: 8,
+        credito: 1,
+    },
+    {
+        nombreMateria: "JavasScript",
+        nota: 6,
+        credito: 5,
+    }
+]
+
+const notasPonderadas = materias.map(function(notasObjeto) {
+    const notasMultiplicadas = notasObjeto.nota * notasObjeto.credito;
+    
+})
+
+const 
